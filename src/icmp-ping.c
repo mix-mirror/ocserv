mix-mirror/ocserv

commit 2aaa287a4c768ebf97de8bb3e0cb3e04ddf02b7c

icmp-ping: removed unnecessary assignments
Signed-off-by: Nikos Mavrogiannopoulos <n.mavrogiannopoulos@gmail.com>

src/icmp-ping.c
@@ -200,9 +200,9 @@ int icmp_ping4(main_server_st * s, struct sockaddr_in *addr1)
 	pkt->icmp_cksum =
 	    in_cksum((unsigned short *) pkt, sizeof(packet1));
 
-	while ((c = sendto(pingsock, packet1, DEFDATALEN + ICMP_MINLEN, 0,
-			   (struct sockaddr *) addr1,
-			   sizeof(*addr1)) == -1) && retry(errno));
+	while (sendto(pingsock, packet1, DEFDATALEN + ICMP_MINLEN, 0,
+		      (struct sockaddr *) addr1,
+		      sizeof(*addr1) == -1) && retry(errno));
 	/* listen for replies */
 
 	now = time(0);
@@ -294,11 +294,10 @@ int icmp_ping6(main_server_st * s,
 	setsockopt(pingsock, SOL_RAW, IPV6_CHECKSUM,
 		   &sockopt, sizeof(sockopt));
 #endif
-	while ((c =
-		sendto(pingsock, packet1,
-		       DEFDATALEN + sizeof(struct icmp6_hdr), 0,
-		       (struct sockaddr *) addr1,
-		       sizeof(*addr1)) == -1) && retry(errno));
+	while (sendto(pingsock, packet1,
+		      DEFDATALEN + sizeof(struct icmp6_hdr), 0,
+		      (struct sockaddr *) addr1,
+		      sizeof(*addr1) == -1) && retry(errno));
 
 	/* listen for replies */
 	now = time(0);

commit a307bda153ff10090f166f6fc980d1756b7720f3

Repair ping-leases option
A misplaced bracket passed 'sizeof(*addr1) == -1' instead of 'sizeof(*addr1)'
to sendto(), causing it to fail. This prevented icmp_ping4() from sending
ICMP echo requests.

Consequently, the 'ping-leases' option has been non-functional since
this bug was introduced in commit 2aaa287a.

Signed-off-by: Grigory Trenin <grigory.trenin@gmail.com>

src/icmp-ping.c
@@ -202,7 +202,7 @@ int icmp_ping4(main_server_st *s, struct sockaddr_in *addr1)
 	pkt->icmp_cksum = in_cksum((unsigned short *)pkt, sizeof(packet1));
 
 	while (sendto(pingsock, packet1, DEFDATALEN + ICMP_MINLEN, 0,
-		      (struct sockaddr *)addr1, sizeof(*addr1) == -1) &&
+		      (struct sockaddr *)addr1, sizeof(*addr1)) == -1 &&
 	       retry(errno))
 		;
 	/* listen for replies */
@@ -300,7 +300,7 @@ int icmp_ping6(main_server_st *s, struct sockaddr_in6 *addr1)
 	setsockopt(pingsock, SOL_RAW, IPV6_CHECKSUM, &sockopt, sizeof(sockopt));
 #endif
 	while (sendto(pingsock, packet1, DEFDATALEN + sizeof(struct icmp6_hdr),
-		      0, (struct sockaddr *)addr1, sizeof(*addr1) == -1) &&
+		      0, (struct sockaddr *)addr1, sizeof(*addr1)) == -1 &&
 	       retry(errno))
 		;
 